db3ceea6a9c89c624b0de93f580b3e70ff92fcf9
Gage LeBlanc <gage.leblanc@optanix.com>
2021-05-10 00:53:38 -0400

    bump version

--- upldr/setup.py
+++ upldr/setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name='upldr',
-    version='2.0.1',
+    version='2.0.2',
     scripts=['upldr'],
     author="Gage LeBlanc",
     author_email="gleblanc@symnet.io",
